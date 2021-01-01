6e9fecbad3e57106c43625644d4eee915e10996f
ldco2016 <renaissance.scholar2012@gmail.com>
2021-01-01 01:01:07 -0600

    first commit

---
+++ src/app/equation/equation.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
+import { MathValidators } from '../math-validators';
+
+@Component({
+  selector: "app-equation",
+  templateUrl: "./equation.component.html",
+  styleUrls: ["./equation.component.css"],
+})
+export class EquationComponent implements OnInit {
+  mathForm = new FormGroup(
+    {
+      a: new FormControl(this.randomNumber()),
+      b: new FormControl(this.randomNumber()),
+      answer: new FormControl(""),
+    },
+    [MathValidators.addition('answer', 'a', 'b')]
+  );
+
+  constructor() {}
+
+  get a() {
+    return this.mathForm.value.a;
+  }
+
+  get b() {
+    return this.mathForm.value.b;
+  }
+
+  ngOnInit() {
+    this.mathForm.statusChanges.subscribe((value) => {
+      if (value === 'INVALID') {
+        return;
+      }
+
+      this.mathForm.setValue({
+        a: this.randomNumber(),
+        b: this.randomNumber(),
+        answer: ''
+      });
+    });
+  }
+
+  randomNumber() {
+    return Math.floor(Math.random() * 10);
+  }
+}
