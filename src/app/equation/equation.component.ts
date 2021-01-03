1c76e72d8c59551ab210793ca6b9bf72454b5702
ldco2016 <renaissance.scholar2012@gmail.com>
2021-01-03 08:39:05 -0600

    Styling Added

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl } from "@angular/forms";
+import { delay, filter, scan } from 'rxjs/operators';
 import { MathValidators } from '../math-validators';
 
 @Component({
@@ -8,6 +9,7 @@ import { MathValidators } from '../math-validators';
   styleUrls: ["./equation.component.css"],
 })
 export class EquationComponent implements OnInit {
+  secondsPerSolution = 0;
   mathForm = new FormGroup(
     {
       a: new FormControl(this.randomNumber()),
@@ -28,10 +30,15 @@ export class EquationComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.mathForm.statusChanges.subscribe((value) => {
-      if (value === 'INVALID') {
-        return;
-      }
+    this.mathForm.statusChanges.pipe(filter(value => value === 'VALID'), delay(200), scan((acc) => {
+      return {
+        numberSolved: acc.numberSolved + 1,
+        startTime: acc.startTime
+      };
+    }, { numberSolved: 0, startTime: new Date() })).subscribe(({ numberSolved, startTime }) => {
+      this.secondsPerSolution = (
+        new Date().getTime() - startTime.getTime()
+      ) / numberSolved / 1000;
 
       this.mathForm.setValue({
         a: this.randomNumber(),
